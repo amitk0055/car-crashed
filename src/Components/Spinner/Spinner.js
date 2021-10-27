170db1c54ebe5c54aaf833c63e4acf6ec2085962
Amit Kumar <amit@callcomm.com>
2021-10-27 11:26:20 +0530

    ui inpv

--- src/Components/Spinner/Spinner.js
+++ src/Components/Spinner/Spinner.js
@@ -1,12 +1,11 @@
-import useSpinner from '../../../Store/SpinnerHook'
 import  './spinner.css'
 
 const Spinner = ({value})=> {
     return(<>
-    { value && value.spinner &&  <div className="fullpage-spinner flx flx-center">
+    { value &&  <div className="fullpage-spinner flx flx-center">
         <div className="loading-spinner-container">
             <div className="spinner-border text-primary" role="status">
-                </div><div className="mt-3 text-dark">{value.message}</div>
+                </div><div className="mt-3 text-dark">{"Loading Please Waits..."}</div>
                 </div>
             </div>}
         </>)
